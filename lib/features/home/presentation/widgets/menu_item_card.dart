f7311bc3d7a5fb79089408e2003a9d5172196f57
CarolineVantim <carolvantim@gmail.com>
2024-11-24 16:57:43 -0300

    feature: starting the system structure

---
+++ lib/features/home/presentation/widgets/menu_item_card.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:sistema_clinico/shared/constants/constants.dart';
+
+class MenuItemCard extends StatelessWidget {
+  const MenuItemCard({
+    super.key,
+    required this.titulo,
+    required this.resourceEnum,
+    required this.route,
+  });
+
+  final String titulo;
+  final ImageResourceEnum resourceEnum;
+  final String route;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(route);
+      },
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: SvgPicture.asset(
+                resourceEnum.path,
+                fit: BoxFit.fitWidth,
+                // height: 75,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
+              child: Text(
+                titulo,
+                style: Theme.of(context).textTheme.bodyLarge,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
